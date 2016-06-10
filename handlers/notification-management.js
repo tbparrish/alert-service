1425c798091c958026fbaced4404a998db03ecf9
Terry Parrish <terry.parrish@guardtime.com>
2016-06-10 16:24:24 -0400

    PER-2179 - Add pagination, sorting, search to getting notifications

--- handlers/notification-management.js
+++ handlers/notification-management.js
@@ -1,7 +1,18 @@
 var INCLUDE = [{ all: true, nested: true }];
 
 on("NotificationFindQuery", function(data){
-    return models.Notification.findAll({where: data, include: INCLUDE});
+    var limit = data.limit ? data.limit : Number.MAX_SAFE_INTEGER;
+    var offset = data.offset ? data.offset : 0;
+    delete data.limit;
+    delete data.offset;
+
+    return models.Notification.findAll({where: data})
+      .then(function(notifications){
+        return models.Notification.findAll({where: data, limit: limit, offset: offset, order: '"createdAt" DESC', include: INCLUDE})
+      .then(function(notifications) {
+        return {totalItems: notifications.length, notifications: notifications };
+      });
+    });
 });
 on("NotificationGetQuery", function(data){
     return models.Notification.findOne({where: data, include: INCLUDE});
