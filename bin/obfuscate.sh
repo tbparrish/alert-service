22b0bbbdbf5d203bb388e4e75d055c99ae1149ae
Anthony Moralez <anthony.moralez@guardtime.com>
2016-06-21 10:29:48 -0700

    fix bug in obfuscate.sh

--- bin/obfuscate.sh
+++ bin/obfuscate.sh
@@ -9,5 +9,5 @@ fi
 for h in handlers/*.js; do echo "var $(basename -s .js $h | tr - _) = require(\"./$(basename $h)\");" >> handlers/entrypoint.js;done
 for h in models/*.js; do echo "var $(basename -s .js $h | tr - _) = require(\"./$(basename $h)\");" >> models/entrypoint.js;done
 $(npm bin)/obfuscator --no-color --strings --out alert-service.js --entry index.js index.js helpers/*.js
-$(npm bin)/obfuscator --strings --no-color --out handlers/handlers.js --entry handlers/entrypoint.js handlers/*.js helpers/*.js lib/*.js && \
+$(npm bin)/obfuscator --strings --no-color --out handlers/handlers.js --entry handlers/entrypoint.js handlers/*.js helpers/*.js && \
 $(npm bin)/obfuscator --no-color --strings --out models/models.js --entry models/entrypoint.js models/*.js
