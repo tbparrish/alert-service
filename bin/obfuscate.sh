b35c3390c721891b06ed1676634e2002d0e049a0
Anthony Moralez <anthony.moralez@guardtime.com>
2016-06-21 10:11:31 -0700

    Updating obfuscation to include all of the things

--- bin/obfuscate.sh
+++ bin/obfuscate.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-$(npm bin)/obfuscator --no-color --strings --out alert-service.js --entry index.js index.js
+if [ -e handlers/entrypoint.js ]; then
+   rm handlers/entrypoint.js;
+fi
+if [ -e models/entrypoint.js ]; then
+   rm models/entrypoint.js;
+fi
+for h in handlers/*.js; do echo "var $(basename -s .js $h | tr - _) = require(\"./$(basename $h)\");" >> handlers/entrypoint.js;done
+for h in models/*.js; do echo "var $(basename -s .js $h | tr - _) = require(\"./$(basename $h)\");" >> models/entrypoint.js;done
+$(npm bin)/obfuscator --no-color --strings --out alert-service.js --entry index.js index.js helpers/*.js
+$(npm bin)/obfuscator --strings --no-color --out handlers/handlers.js --entry handlers/entrypoint.js handlers/*.js helpers/*.js lib/*.js && \
+$(npm bin)/obfuscator --no-color --strings --out models/models.js --entry models/entrypoint.js models/*.js
