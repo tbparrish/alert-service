6a6df29b19665645dbab491b790921003fc98d53
Laurent <laurent.jacquot@guardtime.com>
2016-07-05 12:09:20 -0700

    Adds a socket event when opening or closing a notification

--- handlers/note-management.js
+++ handlers/note-management.js
@@ -12,6 +12,13 @@ on("NoteCreateCommand", function(data){
       return models.Notification.findOne({where: {id: data.notificationId}})
         .then(function(notification){
           return notification.update({status: "Closed", closedTime: Date.now(), closedBy: data.user}, {include: INCLUDE})
+            .finally(function(resp) {
+              models.Notification.findAll({where: { status: 'Open' }})
+                .then(function(notifications){
+                  var totalOpen = notifications.length;
+                  event( 'CloseNotificationEvent', { totalOpen: totalOpen } );
+                });
+              })
         .then(function(notification){
           return command('NotificationGetQuery', {id: notification.id})
           .then(function(notification){
